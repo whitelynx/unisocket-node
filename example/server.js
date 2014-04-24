ce772bd2bb0d17c54951fe7b9de12e30c4eb24d7
Christopher S. Case <chris.case@g33xnexus.com>
2014-04-24 10:59:31 -0500

    Added an example with zero dependencies.

---
+++ example/server.js
@@ -0,0 +1,35 @@
+//----------------------------------------------------------------------------------------------------------------------
+// A simple example server.
+//
+// @module server.js
+//----------------------------------------------------------------------------------------------------------------------
+
+var fs = require('fs');
+var path = require('path');
+var UniSocketServer = require('../unisocket');
+
+//----------------------------------------------------------------------------------------------------------------------
+
+var socketServer = (new UniSocketServer()).listen(4000);
+var logger = socketServer.logger;
+
+// Handle serving of index file
+socketServer.httpServer.on('request', function(request, response)
+{
+    if(request.url == '/' || request.url == '/index.html')
+    {
+        fs.createReadStream(path.resolve('./client/index.html')).pipe(response);
+    } // end if
+});
+
+socketServer.on('connection', function(client)
+{
+    client.on('echo', function(msg, callback)
+    {
+        callback(msg);
+    });
+});
+
+logger.info("Example server started. Listening on port %s", socketServer.httpServer.address().port);
+
+//----------------------------------------------------------------------------------------------------------------------
